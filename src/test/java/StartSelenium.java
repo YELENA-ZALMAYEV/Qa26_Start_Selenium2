b12b69316fbaa04c7c779c56f3cc05859ae5bcd0
YELENA-ZALMAYEV <elenkina73@gmail.com>
2024-06-21 22:15:45 +0300

    add login test

--- src/test/java/StartSelenium.java
+++ src/test/java/StartSelenium.java
@@ -10,14 +10,14 @@ public class StartSelenium {
     @Test
     public void test(){
         wd = new ChromeDriver();
-      //  wd.get("https://telranedu.web.app/home");
+      //  wd.get("https://telranedu.web.app/home"); //open without steps on website
 
-        wd.navigate().to("https://telranedu.web.app/home");
+        wd.navigate().to("https://telranedu.web.app/home"); //with steps
 //        wd.navigate().back();
 //        wd.navigate().forward();
 //        wd.navigate().refresh();
 
-//        wd.close();
-//        wd.quit();
+//        wd.close();//only tab
+//        wd.quit();//all
     }
 }

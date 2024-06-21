758d5b78c16861df7abbb7c5478a457b8e9a385f
YELENA-ZALMAYEV <elenkina73@gmail.com>
2024-06-21 12:39:55 +0300

    test

---
+++ src/test/java/Index.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+
+public class Index {
+    WebDriver wd;
+
+
+    @BeforeClass
+    public  void  setUp(){
+        wd = new ChromeDriver();
+       // wd.get();
+    }
+}

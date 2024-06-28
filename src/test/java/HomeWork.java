b6b8ab55f598445c8dfc11bf93f194a0a28d77d5
YELENA-ZALMAYEV <elenkina73@gmail.com>
2024-06-28 16:35:53 +0300

    cw

--- src/test/java/HomeWork.java
+++ src/test/java/HomeWork.java
@@ -19,6 +19,33 @@ public class HomeWork{
         wd = new ChromeDriver();
         wd.get("https://telranedu.web.app/login");
     }
+
+
+    @Test
+    public void classwork2() {
+        WebElement loginButton = wd.findElement(By.cssSelector("[name='login']"));
+        String text = loginButton.getText();
+        System.out.println(text);
+
+
+        WebElement form = wd.findElement(By.xpath("//form"));
+        String textForm = form.getText();
+        System.out.println("******************");
+        System.out.println(textForm);
+
+        WebElement html = wd.findElement(By.tagName("html"));
+        String textAll = html.getText();
+        System.out.println("******************");
+        System.out.println(textAll);
+
+        WebElement br = wd.findElement(By.tagName("br"));
+        System.out.println("******************");
+        System.out.println("text br--->" +br.getText());
+    }
+
+
+
+
     @Test
     public  void  classwork(){
         //parent
